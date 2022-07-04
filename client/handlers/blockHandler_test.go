215148b66215e84b53a453151dd4463200670823
arthur-zeus <arthur.cardoso@zeusagro.com>
2022-07-04 17:29:00 -0300

    Starting test blocks function

--- client/handlers/blockHandler_test.go
+++ client/handlers/blockHandler_test.go
@@ -1,8 +1,24 @@
 package handlers
 
-import "testing"
+import (
+	block "go-grpc/commons/pb"
+	"testing"
+)
+
+var blockTest = &block.ResponseBlock{
+	Id:       "F1",
+	ParentId: "C0",
+}
 
 func TestGetBlockById(t *testing.T) {
 	//	todo: test existent block
 	//	todo: test nonexistent block
+
+	t.Run("Getting existent block", func(t *testing.T) {
+
+	})
+
+	t.Run("Getting non existent block", func(t *testing.T) {
+
+	})
 }
