cfac66728734f534bf1ebb6860fe00aa2c4210b9
arthur-zeus <arthur.cardoso@zeusagro.com>
2022-07-06 17:04:31 -0300

    updating flags

--- commons/blocksFlags.go
+++ commons/blocksFlags.go
@@ -5,5 +5,5 @@ import "flag"
 var (
 	PORTService = flag.String("port blocks service", ":8080", "Blocks service server port")
 
-	PORTClient = flag.String("port blocks client", ":8000", "Blocks client server port")
+	PORTClient = flag.String("port blocks client", ":8080", "Blocks client server port")
 )
