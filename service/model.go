89f77efed8570400e861b59637d601c61df8e927
lucas-mezencio <lucas.santana@zeusagrotech.com>
2022-07-13 09:49:06 -0300

    fix query syntax

--- service/model.go
+++ service/model.go
@@ -61,25 +61,25 @@ func (server *Server) GetBlockById(context context.Context, req *block.RequestID
 }
 
 func getQueryBlock() string {
-	return "select (a_temp.data_timestamp) as data_timestamp, (a_temp.created_timestamp) as created_timestamp," +
-		"a_temp.data_inst as temp_inst, a_temp.data_min as temp_min, a_temp.data_max as temp_max," +
-		"a_prec.data_inst as prec_inst, a_prec.data_min as prec_min, a_prec.data_max as prec_max" +
-		"from `" + tableName + "` a_temp " +
-		"inner join (" +
-		"select data_timestamp, created_timestamp, data_inst, data_min, data_max, source_id" +
-		"from `" + tableName + "`" +
-		`where date(created_timestamp) = "2022-06-28"` +
-		"and datatype_id = " + precipitationDataTypeId +
-		"and source_type_id = 3" +
-		"and source_subtype_id = 16" +
-		"and source_id = @source_id" +
-		") as a_prec" +
-		"on a_temp.source_id = a_prec.source_id" +
-		`where date(a_temp.created_timestamp) = "2022-06-28"` +
-		"and a_temp.datatype_id = " + temperatureDataTypeId +
-		"and a_temp.source_type_id = 3" +
-		"and a_temp.source_subtype_id = 16" +
-		"and a_temp.source_id = @source_id" +
-		"order by a_temp.created_timestamp desc, a_prec.created_timestamp desc" +
-		"limit 1"
+	return "select (a_temp.data_timestamp) as data_timestamp, (a_temp.created_timestamp) as created_timestamp, " +
+		" a_temp.data_inst as temp_inst, a_temp.data_min as temp_min, a_temp.data_max as temp_max, " +
+		" a_prec.data_inst as prec_inst, a_prec.data_min as prec_min, a_prec.data_max as prec_max " +
+		" from `" + tableName + "` a_temp " +
+		" inner join ( " +
+		" select data_timestamp, created_timestamp, data_inst, data_min, data_max, source_id " +
+		" from `" + tableName + "` " +
+		` where date(created_timestamp) = "2022-06-28" ` +
+		" and datatype_id = " + precipitationDataTypeId +
+		" and source_type_id = 3 " +
+		" and source_subtype_id = 16 " +
+		" and source_id = @source_id " +
+		" ) as a_prec " +
+		" on a_temp.source_id = a_prec.source_id " +
+		` where date(a_temp.created_timestamp) = "2022-06-28" ` +
+		" and a_temp.datatype_id = " + temperatureDataTypeId +
+		" and a_temp.source_type_id = 3 " +
+		" and a_temp.source_subtype_id = 16 " +
+		" and a_temp.source_id = @source_id " +
+		" order by a_temp.created_timestamp desc, a_prec.created_timestamp desc " +
+		" limit 1 "
 }
